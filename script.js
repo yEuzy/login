0e0963a7c3402eb4a1713bebe744b220b438a262
yEuzy <kallewfelip100@gmail.com>
2023-12-17 00:46:54 -0300

    vai

---
+++ script.js
@@ -0,0 +1,46 @@
+
+
+var logi = [
+    {
+        login: 'kallew',
+        sen: 'kallew08'
+    },
+    {
+        login: 'a',
+        sen: 'a'
+    },
+    {
+        login: 'mriarta',
+        sen: '200404'
+    }
+]
+
+
+var botao = document.getElementById('botao')
+
+
+
+function falar() {
+    var usuarios = document.getElementById('usuario').value
+    var senha = document.getElementById('senha').value
+    var logou = false
+
+    for(var i in logi){
+        if (usuarios == logi[i].login && senha == logi[i].sen){
+            logou = true
+            break
+        }
+    }
+
+    if (logou == true){
+        location.href = ('index.html')
+    }
+    else{
+        alert('erro')
+    }
+    
+
+}
+
+
+botao.addEventListener('click', falar)
