d2949474aa993ff809305971b58c7adfb0065a21
yEuzy <kallewfelip100@gmail.com>
2023-12-17 21:05:39 -0300

    vai

--- criar.js
+++ criar.js
@@ -1,15 +1,33 @@
 var botao = document.getElementById("botao")
 
 function criar(){
+    
+    // criando uma variavel para a div pai
+    var pai = document.getElementById("tweetar")
+
+    //criando a area para os tweets
+    var tweets = document.getElementById('tweets')
 
 
+    //criando uma variavel para o input do texto
     var texto = document.getElementById("texto").value
+
+    //criando variavel para a div
     var criardiv = document.createElement('div')
+
+    //dando uma class para a div
     criardiv.classList.add("divtt")
+
+    //criando uma variavel com co conteudo do input
     var conteudo = document.createTextNode(`${texto}`)
+
+    //inserindo o conteudo na div
     criardiv.appendChild(conteudo)
-    var div1 = document.getElementById('vai')
-    document.body.insertBefore(criardiv,div1)
+
+    //criando a div com conteudo dentro da div pai
+    tweets.appendChild(criardiv)
+
+    
 }
 
 botao.addEventListener('click', criar)
