6e968801040e084bad89b4ea5566d7ea40e454cb
yEuzy <kallewfelip100@gmail.com>
2023-12-17 23:46:15 -0300

    vai

--- mobile.js
+++ mobile.js
@@ -4,13 +4,18 @@ var normal = document.getElementById("tweetar")
 
 var ladol = false
 
+
+
 function abrirop(){
     document.getElementById("lbar").style.display = 'block'
     ladol = true
 }
 
 function fecharop(){
-    document.getElementById("lbar").style.display = 'none'
+    var tlado = document.querySelector("body").offsetWidth
+    if(tlado <= 450){
+        document.getElementById("lbar").style.display = 'none'
+    }
 }
 
 botper.addEventListener('click', abrirop)
